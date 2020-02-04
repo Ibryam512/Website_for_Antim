181f6e858dfdf36cb0e681af6a66fb635826989a
bamko2003 <bamko2003@gmail.com>
2020-02-04 12:05:51 +0200

    finished the uploading and showing the posts, except uploading image and date

--- add.php
+++ add.php
@@ -3,11 +3,21 @@
 	function Add($title, $desc, $price, $type, $date)
 	{
 		$conn = OpenCon();
+		$typ;
+		if($type == "Обява")
+		{
+			$typ = true;
+		}
+		else
+		{
+			$typ = false;
+		}
 		$sql = "INSERT INTO items
 					(title, description, price, type, date)
-					VALUES ('$title', '$desc', $price, '$type', '$date')";
+					VALUES ('$title', '$desc', $price, '$typ', '$date')";
 		$conn->query($sql);
 		CloseCon($conn);
+		header("Location: index.php");
 	}
-	Add($_POST['title'], $_POST['desc'], $_POST['price'], $_POST['type'], $_POST['date']);
+	Add($_POST['title'], $_POST['desc'], $_POST['price'], $_POST['group3'], $_POST['date']);
 ?>
---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="Antim I, Antim">
+		<meta name="autors" content="Ibryam Ibryamov, Ventsislav Nenov">
+		<title>Антималник</title>
+		<link rel="stylesheet" href="css\style.css">
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+		<script src="js/jquery.js"></script>
+		<script src="js/materialize.js"></script>
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<div id="menu">
+			<div id="menu">
+			<ul>
+				<il id="options"><a href="Profile.html"><button style="border-radius: 5000px;cursor: pointer;background-color:initial;border: initial;"><img style="border-radius: 5000px;" src="pic/profilePic.png"height="44" width="44"></button></a></il> 
+				<li id="options"><a href="team.html">За нас</a></li>
+				<li id="options"><a href="questions.html">Въпроси</a></li>
+				<li id="options"><a href="lost_things.html">Изгубени вещи</a></li>
+				<li id="options"><a style="background-color: white;" href="index.html">Сергия</a></li>
+				<li id="options"><a class="btn-floating btn-medium waves-effect waves-light blue pulse" href="add.html" style="margin-top: 10%;"><i class="material-icons">+</i></a></li>
+				<li id="image"><img src="pic/image.png" height="45" width="45"></li>
+			</ul>
+		</div>
+		<table style="width:100%">
+			<?php
+				include 'connect.php';
+				
+				function Show()
+				{
+					$conn = OpenCon();
+					$sql = "SELECT * FROM items
+							WHERE type=TRUE
+							ORDER BY ID DESC;";
+					$result = $conn->query($sql);
+					$smth = 0;
+					while($row = $result->fetch_assoc())
+					{
+						$title = $row["title"];
+						$desc = $row["description"];
+						if($smth % 2 == 0)
+						{
+							echo"<tr>
+									<td><div id='post' class='card'>
+									<div class='card-image waves-effect waves-block waves-light'>
+										<img style='object-fit: contain;' src='pic/image.png' height='100'>
+									</div>
+									<div class='card-content'>
+										<span class='card-title activator grey-text text-darken-4'>$title<i class='material-icons right'>...</i></span>
+										<p><a href='detail.html'>Пълен размер</a></p>
+									</div>
+									<div class='card-reveal'>
+										<span class='card-title grey-text text-darken-4'>Card Title<i class='material-icons right'>затвори</i></span>
+										<p>$desc</p>
+									</div></td>";
+							$smth++;		
+						}
+						else
+						{
+							echo"	<td><div id='post' class='card'>
+									<div class='card-image waves-effect waves-block waves-light'>
+										<img style='object-fit: contain;' src='pic/image.png' height='100'>
+									</div>
+									<div class='card-content'>
+										<span class='card-title activator grey-text text-darken-4'>$title<i class='material-icons right'>...</i></span>
+										<p><a href='detail.html'>Пълен размер</a></p>
+									</div>
+									<div class='card-reveal'>
+										<span class='card-title grey-text text-darken-4'>Card Title<i class='material-icons right'>затвори</i></span>
+										<p>$desc</p>
+									</div></td></tr>";
+							$smth++;
+						}
+					}
+				}
+				Show();
+			?>
+		</table>
+	</body>
+</html>
