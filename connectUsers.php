f4e5a69d95b196587d3fad4fc30485d01f10de35
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-06 17:22:21 +0200

    end tiping

--- connectUsers.php
+++ connectUsers.php
@@ -6,4 +6,17 @@ $email = stripcslashes($email);
 $password = stripcslashes($password);
 $email = mysql_real_escape_string($email);
 $password = mysql_real_escape_string($password);
+
+mysql_connect("localhost","root","");
+mysql_select_db("login");
+
+$result = mysql_query("select * from users where email = '$email' and  password = '$password'")
+or die("Failed to query database");
+$row = mysql_fetch_array($result);
+if($row['email'] == $email && $row['password'] == $password){
+        echo "loged in".$row['email'];
+}
+else{
+    echo "Failed";
+}
 ?>
