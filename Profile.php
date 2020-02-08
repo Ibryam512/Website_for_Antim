e8af23a91c001a28394cd836182f2aee0a96d1aa
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-09 00:11:12 +0200

    barain deat

---
+++ Profile.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="Antim I, Antim">
+		<meta name="autors" content="Ibryam Ibryamov, Ventsislav Nenov">
+		<title>Антималник</title>
+		<link rel="stylesheet" href="css\style.css">
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+		<script src="js/jquery.js"></script>
+		<script src="js/materialize.js"></script>
+		<script src="js/script.js"></script>
+    </head>
+    <body>
+        <div id="menu">
+			<div id="menu">
+			<ul>
+				<il id="options"><a href="Profile.html"><button style="border-radius: 5000px;cursor: pointer;background-color:initial;border: initial;"><img style="border-radius: 5000px;" src="pic/profilePic.png"height="44" width="44"></button></a></il> 
+				<li id="options"><a href="team.html">За нас</a></li>
+				<li id="options"><a href="questions.html">Въпроси</a></li>
+				<li id="options"><a href="lost_things.php">Изгубени вещи</a></li>
+				<li id="options"><a href="index.php">Сергия</a></li>
+				<li id="image"><img src="pic/image.png" height="45" width="45"></li>
+			</ul>
+        </div>
+        <div style="text-align: center;">
+            <img style="border-radius: 5000px;" src="Pic/profilePic.png" height="400"width="400">
+            <div>
+                <button style="border-radius: 5000px;cursor: pointer;background-color: initial;border: initial;" ><img style="border-radius: 5000000px;" src="pic/Edit.png" height="40"></button>
+            </div>
+        </div>
+        <div style="text-align: center;">
+            <label for="disabled">Име</label>
+            <input style="text-align: center; font-size: xx-large;" disabled value="Иван" id="disabled" type="text" class="validate">
+        </div>
+        <div style="text-align: center;">
+            <label for="disabled">Презиме</label>
+            <input style="text-align: center;font-size: xx-large;" disabled value="Иван" id="disabled" type="text" class="validate">
+        </div>
+        <div style="text-align: center;">
+            <label for="disabled">Фамилия</label>
+            <input style="text-align: center;font-size: xx-large;" disabled value="Иван" id="disabled" type="text" class="validate">
+        </div>
+        <div style="text-align: center;">
+            <label for="disabled">Парола</label>
+            <input style="text-align: center;font-size: xx-large;" disabled value="@@@@@@" id="disabled" type="text" class="validate">
+        </div>
+        <div style="text-align: center;margin-bottom: 50;">
+            <label for="disabled">Имейл</label>
+            <input style="text-align: center;font-size: xx-large;" disabled value="аз.аз@аз.аз" id="disabled" type="text" class="validate">
+        </div>
+    </body>
+</html>
--- connectUsers.php
+++ connectUsers.php
@@ -5,20 +5,21 @@ $password="";
 $db="login";
 $som=mysqli_connect($host,$user,$password);
 mysqli_select_db($som,$db);
-
+echo"text"
 if(isset($_POST['email'])){
         $email=$_POST['email'];
         $password=$_POST['password'];
-        $sql="select * from users where e-mail='".$email."' AND pass='".$password."'limit 1";
+        $sql="SELECT * FROM users WHERE e-mail='".$email."' AND pass='".$password."'LIMIT 1";
 
         $result=mysql_query($sql);
+        
         if(mysql_num_rows($result) == 1){
-          echo"Logged in !!!";
-          exit();
+          echo"maikoooo";
+          
         }
         else{
-          echo"Incorrect";
-          exit();
+         echo"miau";
+         
         }
 }
 ?>
