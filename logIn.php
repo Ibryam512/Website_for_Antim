51b82ee8ed240b9427a8e5684622aec117a434ed
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-08 22:41:23 +0200

    BAHTIIIII

--- logIn.php
+++ logIn.php
@@ -1,3 +1,27 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="login";
+$som=mysqli_connect($host,$user,$password);
+mysqli_select_db($som,$db);
+
+if(isset($_POST['email'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $sql="select * from users where e-mail='".$email."' AND pass='".$password."'limit 1";
+
+        $result=mysql_query($sql);
+        if(mysql_num_rows($result) == 1){
+          echo"Logged in !!!";
+          exit();
+        }
+        else{
+          echo"Incorrect";
+          exit();
+        }
+}
+?>
 <html>
     <head>
 		<meta charset="utf-8">
@@ -13,19 +37,19 @@
 	<body>
         <h1 style="text-align:center">Моето училище</h1>
 		<div class="row">
-            <form action="connectUsers.php" method="POST" class="col s12" >
+            <form action="#" method="POST" class="col s12" >
               <div class="row">
-                <div class="input-field col s12" style="text-align: center;">
+                <div class="input-field col s12" style="text-align: center; margin-top: 10;">
                   <input placeholder="Имейл" id="email" type="email" class="validate">
                 </div>
               </div>
               <div class="row"style="text-align: center;">
-                <div class="input-field col s12" style="text-align: center;">
+                <div class="input-field col s12" style="text-align: center;margin-top: 10;">
                   <input placeholder="Парола" id="password" type="password" class="validate">
                 </div>
               </div>
               <div class="row">
-                  <div class="input-field col s12" style="text-align: center;">
+                  <div class="input-field col s12" style="text-align: center;margin-top: 10;">
                       <button style="text-align: center;">Напред</button>
                   </div>
               </div>
