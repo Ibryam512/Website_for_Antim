a8701201d5794c5af6ce6d2bdc0cd437de5bf2f6
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-09 19:15:19 +0200

    better

--- logIn.php
+++ logIn.php
@@ -1,36 +1,22 @@
-
-<html>
-    <head>
-		<meta charset="utf-8">
-		<meta name="keywords" content="Antim I, Antim">
-		<meta name="autors" content="Ibryam Ibryamov, Ventsislav Nenov">
-		<title>Въпроси</title>
-		<link rel="stylesheet" href="css\style.css">
-		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-		<script src="js/jquery.js"></script>
-		<script src="js/materialize.js"></script>
-		<script src="js/script.js"></script>
-	</head>
-	<body>
-        <h1 style="text-align:center">Моето училище</h1>
-		<div class="row">
-            <form action="connectUsers.php" method="POST" class="col s12" >
-              <div class="row">
-                <div class="input-field col s12" style="text-align: center; margin-top: 10;">
-                  <input placeholder="Имейл" id="email" type="email" class="validate">
-                </div>
-              </div>
-              <div class="row"style="text-align: center;">
-                <div class="input-field col s12" style="text-align: center;margin-top: 10;">
-                  <input placeholder="Парола" id="password" type="password" class="validate">
-                </div>
-              </div>
-              <div class="row">
-                  <div class="input-field col s12" style="text-align: center;margin-top: 10;">
-                      <button style="text-align: center;">Напред</button>
-                  </div>
-              </div>
-            </form>
-          </div>
-    </body>
-</html>
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="login";
+$com=mysqli_connect($host,$user,$password);
+mysqli_select_db($som,$db);
+if(isset($_GET['email'])&&isset($_GET['password'])){
+        $email=$_GET['email'];
+        $password=$_GET['password'];
+        $sql="SELECT * FROM users WHERE e-mail='".$email."' AND pass='".$password."'LIMIT 1";
+        $result=mysqli_query($con,$sql);
+        if(mysqli_num_rows($result) == 1){
+          echo"Logged in !!!";
+          exit();
+        }
+        else{
+          echo"Incorrect";
+          exit();
+        }
+}
+?>
