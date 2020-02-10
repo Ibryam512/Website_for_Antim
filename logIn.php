bbff26cc15cca938d431ff18fe6f2d639d13d5ae
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-11 01:37:36 +0200

    login ended !!!

--- logIn.php
+++ logIn.php
@@ -3,20 +3,33 @@ $host="localhost";
 $user="root";
 $password="";
 $db="login";
-$com=mysqli_connect($host,$user,$password);
-mysqli_select_db($som,$db);
-if(isset($_GET['email'])&&isset($_GET['password'])){
-        $email=$_GET['email'];
-        $password=$_GET['password'];
-        $sql="SELECT * FROM users WHERE e-mail='".$email."' AND pass='".$password."'LIMIT 1";
-        $result=mysqli_query($con,$sql);
-        if($result != null){
-          echo"Logged in !!!";
-          exit();
+$conn=new mysqli($host,$user,$password,$db);
+if(!empty($_POST['email'])&&!empty($_POST['password'])){
+        if($conn->connect_error){
+           die('Conn failed !!!! '.$conn->connect_error);
+       }
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $sql="SELECT * FROM users";
+        $result = $conn->query($sql);
+        $int = 0;
+        $mom = false;
+        while($int < $row=$result->num_rows){
+          $row = $result->fetch_assoc();
+          $dbEmail = $row['e-mail'];
+          $dbPassword = $row['pass'];
+          // ima takuv
+          if($dbEmail === $email  &&  $dbPassword === $password){
+              header("Location: Profile.html");
+              $mom = true;
+          }
+          $int++;
         }
-        else{
-          echo"Incorrect";
-          exit();
+        /// Nqma takuv
+        if($mom == false){
+          header("Location: login.html");
+          echo "<script type='text/javascript'>alert('Wrong');</script>";
         }
 }
+
 ?>
