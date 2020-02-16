a358851a00fbb6db63c0e30f96dbdede282f0cbe
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-02-17 01:48:57 +0200

    offff finaly work CHprofile.php

--- CHprofile.php
+++ CHprofile.php
@@ -3,38 +3,23 @@
 	session_start();
     $conn=OpenCon();
 		if(isset($_POST['photo'])){
-			$idDB=$_SESSION['id'];
-			$image=addslashes($_FILES['photo']['tmp_name']);
-			$name=addcslashes($_FILES['image']['name']);
-			$image= file_get_contents($image);
-			$image=base64_encode($image);
-			$sql="UPDATE `users` SET `id`='$dbID',`pic`='$image' WHERE 1";
-			$result=mysqli_query($sql,$conn);
+			
 		}
 		else if(isset($_POST['save'])){
 			if(!empty($_POST['name'])&&!empty($_POST['secName'])&&!empty($_POST['lastName'])&&!empty($_POST['password'])&&!empty($_POST['email'])){
-				$sql1="SELECT * FROM users";
-      			$result = $conn->query($sql1);
-      			$id = 0;
-        		$mom = false;
-        		while($id < $row=$result->num_rows){
-          		$row = $result->fetch_assoc();
-          		$bdEmail=$row['e-mail'];
-          		if($email==$bdEmail){
-              		$mom=true;
-          		}
-          		$id++;
-        		}
+				$mom=false;
         		if($mom == false){
 					$dbID = $_SESSION['id'];
-                	$email=$_POST['email'];
+					$emailii=$_POST['email'];
+					$email=hash('sha256',$_POST['email']."Ibrqm,Venci");
                 	$passii=$_POST['password'];
 					$pass=hash('sha256',$_POST['password']."Ibrqmov,Nenov");
 					$name=$_POST['name'];
 					$secName=$_POST['secName'];
 					$lastName=$_POST['lastName'];
-					////skildfjhgjlkdsfhgjlkdsfhglkjfdhsglkjsd
-					$sql="UPDATE `users` SET 'id'='$idID',`e-mail`='$email',`pass`='$pass',`name`='$name',`secName`='$secName',`lastName`='$lastName'";
+					if(strlen($passii)>=6){
+						////skildfjhgjlkdsfhgjlkdsfhglkjfdhsglkjsd
+					$sql="UPDATE `users` SET `id`=$dbID,`e-mail`='$email',`pass`='$pass',`name`='$name',`secName`='$secName',`lastName`='$lastName' WHERE 1";
                 	$result = $conn->query($sql);
                 	unset($_SESSION['id']);
                 	unset($_SESSION['email']);
@@ -44,12 +29,16 @@
                 	unset($_SESSION['lastName']);
                 	$dbID++;
                 	$_SESSION['id']=$dbID;
-                	$_SESSION['email']=$email;
+                	$_SESSION['email']=$emailii;
                 	$_SESSION['password']=$passii;
                 	$_SESSION['name']=$name;
                 	$_SESSION['secName']=$secName;
                 	$_SESSION['lastName']=$lastName;
 					header("Location: Profile.php");
+					}
+					else{
+						header("Location: Profile.php");
+					}
 				}
 				else{
 					header("Location: CHprofile.html");
